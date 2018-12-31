f954a6a6c42b50ccbed092dbe7ec34bfd5457702
WillFoster95 <w.foster5764@student.leedsbeckett.ac.uk>
2018-12-31 14:39:13 +0000

    added creat list activity

--- app/src/main/java/com/example/will/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/will/shoppinglist/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.will.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final int IS_SUCCESS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         Button createList;
         createList = findViewById(R.id.createList);
+        createList.setOnClickListener(new View.OnClickListener()                                    //OnClick listener for the  Next button
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intentCreateList = NewListActivity.newIntent(MainActivity.this);
+                startActivityForResult(intentCreateList, IS_SUCCESS);
+
+            }
+        });
+
+        Button viewLists;
+        viewLists = findViewById(R.id.createList);
+        viewLists.setOnClickListener(new View.OnClickListener()                                    //OnClick listener for the  Next button
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
     }
 }
